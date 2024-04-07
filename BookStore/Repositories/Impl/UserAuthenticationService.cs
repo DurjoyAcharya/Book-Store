7d4b566b4a0167999b2dd5c5f21746c9106f1113
DurjoyAcharya <da-durjoy@outlook.com>
2024-04-07 18:01:13 +0600

    login done

--- BookStore/Repositories/Impl/UserAuthenticationService.cs
+++ BookStore/Repositories/Impl/UserAuthenticationService.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using BookStore.Models.DTO;
 using BookStore.Repositories.Abstract;
 using Microsoft.AspNetCore.Identity;
@@ -19,9 +20,53 @@ public class UserAuthenticationService:IUserAuthenticationService
     }
 
 
-    public Task<Status> LoginAsync(Login loginModel)
+    public async Task<Status> LoginAsync(Login loginModel)
     {
-        throw new NotImplementedException();
+        var status = new Status(0, "Init");
+        if (loginModel.Username != null)
+        {
+            var user = await UserManager.FindByNameAsync(loginModel.Username);
+            if (user==null)
+            {
+                status = status with { StatusCode = 0 };
+                status = status with { Notify = "Invalid Username" };
+                return status;
+            }
+
+            if (loginModel.Password != null && !await UserManager.CheckPasswordAsync(user,loginModel.Password))
+            {
+                status = status with { StatusCode = 0 };
+                status = status with { Notify = "Invalid Password" };
+                return status;
+            }
+            var SignInResult = await SignInManager.PasswordSignInAsync(user, loginModel.Password, false, true);
+            if (SignInResult.Succeeded)
+            {
+                var userRoles = await UserManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Name)
+                };
+                foreach (var userRole in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role,userRole));
+                }
+
+                status = status with { StatusCode = 1 };
+                status = status with { Notify = "Login Successfully" };
+            }else if (SignInResult.IsLockedOut)   
+            {
+                status = status with { StatusCode = 0 };
+                status = status with { Notify = "Login LockOut" };
+            }
+            else
+            {
+                status = status with { StatusCode = 0 };
+                status = status with { Notify = "Error In Logging..." };
+            }
+        }
+
+        return status;
     }
 
     public Task LogOutAsync()
